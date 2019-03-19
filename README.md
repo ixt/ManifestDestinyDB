fe736ddd0ce2844bcd14862acd9c511906dca3c7
NfN Orange <orange@ff4500.red>
2019-03-19 13:03:11 +0000

    Adding groundwork for traffic

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 Automated Repository of records for manifest destiny
 
 Not in anyway complete yet 
+
+This project uses data from:
+
+    Lyngs, U., & Binns, R. (2018, April 27). 
+    WebSci’18: Third party tracking in the mobile ecosystem. 
+    https://doi.org/10.17605/OSF.IO/4NU9E
+
+    Abbas Razaghpanah, Arian Akhavan Niaki, Narseo Vallina-Rodriguez, Srikanth Sundaresan, 
+    Johanna Amann, and Phillipa Gill. 
+    2017. "Studying TLS Usage in Android Apps", 
+    ACM International Conference on emerging Networking EXperiments and Technologies (CoNEXT) 2017
---
+++ packets/dig.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Go through a csv of package names and IP's resolve each ip to a PTR DNS record and look up the ip in GeoLiteCity db
+GEOLITE="/usr/share/GeoIP/GeoLite2-City_20190319/GeoLite2-City.mmdb"
+# IP="31.13.81.13"
+TEMP=$(mktemp)
+while read entry; do
+    IFS=, read -a line <<< "$entry"
+    PACKAGE=$(echo ${line[0]} | xargs echo )
+    IP=$(echo ${line[1]} | xargs echo)
+    PTRDOMAIN=$(dig -x $IP \
+        +noquestion +norrcomments +nottlid +noadditional \
+        +nocl +nocmd +nostats +nocrypto \
+        | grep -A1 "ANSWER SECTION" \
+        | tail -1 \
+        | sed -e "s/^.*PTR[[:space:]]*//g" )
+
+    CITY=$(mmdblookup -f $GEOLITE --ip $IP city names en 2>/dev/null | cut -d'"' -f2 | xargs echo) 
+    COUNTRY=$(mmdblookup -f $GEOLITE --ip $IP country names en 2>/dev/null | cut -d'"' -f2 | xargs echo)
+    CONTINENT=$(mmdblookup -f $GEOLITE --ip $IP continent names en | cut -d'"' -f2 | xargs echo )
+    LONG=$(mmdblookup -f $GEOLITE --ip $IP location longitude 2>/dev/null | xargs echo | cut -d" " -f1)
+    LAT=$(mmdblookup -f $GEOLITE --ip $IP location latitude 2>/dev/null | xargs echo | cut -d" " -f1)
+
+cat <<EOF> $TEMP
+{ 
+    "package": "$PACKAGE",
+    "ptr_domain": "${PTRDOMAIN:-"N/A"}",
+    "ip_address": "$IP",
+    "loc_city": "${CITY:-"N/A"}",
+    "loc_country": "${COUNTRY:-"N/A"}", 
+    "loc_continent": "${CONTINENT:-"N/A"}",
+    "loc_long": "${LONG:-"N/A"}", 
+    "loc_lat": "${LAT:-"N/A"}"
+}
+EOF
+cp $TEMP handshakes/$PACKAGE.$IP.json
+done < handshakes.csv
+rm $TEMP
